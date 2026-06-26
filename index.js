c1c2a4c34f8b499d90d670b0641a6aab2c99d0d8
Karthik A N <ankarthik657@gmail.com>
2026-06-26 16:44:33 +0530

    Fix: Update CORS origin to correct Vercel URL

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ const Analytic     = require('./routes/analyticsroutes');
 
 connectDB();
 
-const app = express();
-
 const corsOptions = {
-  origin:              ['https://shopify-one.vercel.app', 'http://localhost:3000'],
-  credentials:        true,
-  methods:            ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders:     ['Content-Type', 'Authorization'],
+  origin: ['https://shopif-one.vercel.app', 'http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));  // ← ADD THIS LINE!
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
